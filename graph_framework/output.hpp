a15cbecddf9a7347a08ba9a3b6c3bb1f6e3a68d8
cianciosa <m4c@ornl.gov>
2025-03-19 13:37:31 -0400

    Add implementation for mpex fields.

--- graph_framework/output.hpp
+++ graph_framework/output.hpp
@@ -23,7 +23,14 @@ namespace output {
 ///  @param[in] status Error status code.
 //------------------------------------------------------------------------------
     static void check_error(const int status) {
+#ifdef NDEBUG
+        if (status) {
+            std::cerr << nc_strerror(status) << std::endl;
+            exit(status);
+        }
+#else
         assert(status == NC_NOERR && nc_strerror(status));
+#endif
     }
 
 //------------------------------------------------------------------------------
@@ -85,7 +92,9 @@ namespace output {
 ///  @brief Destructor.
 //------------------------------------------------------------------------------
         ~result_file() {
+            sync.lock();
             check_error(nc_close(ncid));
+            sync.unlock();
         }
 
 //------------------------------------------------------------------------------
